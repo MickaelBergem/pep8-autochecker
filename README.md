c4e945807a15eb1f02cef74fa6c5ecc16130fbaf
Mickaël <mickael@securem.eu>
2014-08-22 00:08:40 +0200

    Import Django with bootstrap homepage

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ pep8-autochecker
 ================
 
 Automated PEP8 review of Python projects
+
+# Installation
+
+Run the following commands :
+
+    virtualenv env # If you don't want to use a virtualenv, go directly to the `pip install` command
+    source bin/env/activate
+    pip install -r requirements
+
---
+++ pep8autochecker/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+                       url(r'^$',
+                           TemplateView.as_view(template_name='home.html'),
+                           name='home'),
+                       #url(r'^admin/', include(admin.site.urls)),
+                       )
