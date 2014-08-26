d60bdc73f28d09967fac30bb6eed0c89ef65679e
Mickaël Bergem <mickaelb@theodo.fr>
2014-08-26 19:12:29 +0200

    Add custom project_list

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from bsct.urls import URLGenerator
 
@@ -7,5 +8,8 @@ from models import Project
 bsct_patterns_project = URLGenerator(Project).get_urlpatterns(paginate_by=10)
 
 urlpatterns = patterns('',
+                       url('project/$',
+                           ListView.as_view(model=Project, template_name="projects/project_list.html"),
+                           name='project_list'),
                        url('', include(bsct_patterns_project)),
                        )
